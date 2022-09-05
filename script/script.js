5a56312b7f3159471c7da1367f35ee9f7c538591
ihccenen <wirkz01@gmail.com>
2022-09-05 18:40:29 -0300

    Added random bg color to the first 10 div

--- script/script.js
+++ script/script.js
@@ -1,33 +1,50 @@
-function changeColor(e) {
-    e.target.classList.add('div-mouseover');
+function createSquareDiv(num = 16) {
+    for (let i = 0; i < (num * num); i++) {
+        const div = document.createElement('div');
+        div.classList.add('div');
+        div.style.minHeight = 100 / num + '%';
+        div.style.minWidth = 100 / num + '%';
+        div.addEventListener('mouseover', changeColor);
+        container.appendChild(div);
+    }
 }
 
 function changeSquareNumber() {
     const allSquareDiv = document.querySelectorAll('.div') 
-    squareDiv = prompt('New number of squares per side. Maximum 100.');
-
+    let squareDiv = prompt('New number of squares per side. Maximum 100.');
+    
     while (squareDiv > 100) {
         squareDiv = prompt('Too many. Maximum 100.')
     }
-
+    
     btn.textContent = `Change the number of squares per side. Current: ${squareDiv}x${squareDiv}`;
-
+    
     allSquareDiv.forEach(div => div.remove());
     
-    for (let i = 0; i < (squareDiv * squareDiv); i++) {
-        const div = document.createElement('div');
-        div.classList.add('div');
-        container.appendChild(div);
-        div.style.height = 100 / squareDiv + '%';
-        div.style.width = 100 / squareDiv + '%';
-        div.addEventListener('mouseover', changeColor);
+    createSquareDiv(squareDiv);
+}
+
+function changeColor(e) {
+    if (!e.target.classList.contains('color')) {
+        let red = Math.round(Math.random() * 225) + 30;
+        let green = Math.round(Math.random() * 225) + 30;
+        let blue = Math.round(Math.random() * 225) + 30;
+
+        e.target.classList.add('color');
+        divColors.splice(0, 3, red - (red * percent), green - (green * percent), blue - (blue * percent));
+
+        e.target.style.backgroundColor = `rgb(${divColors[0]}, ${divColors[1]}, ${divColors[2]})`;
+
+        percent += 0.10;
     }
 }
 
 const bod = document.querySelector('body');
 const container = document.createElement('div');
 const btn = document.createElement('button');
-let squareDiv = 16;
+const div = Array.from(document.querySelectorAll('.div'));
+const divColors = [];
+let percent = 0;
 
 container.classList.add('container');
 btn.appendChild(document.createTextNode('Change the number of squares per side. Current: 16x16'));
@@ -36,13 +53,4 @@ btn.addEventListener('click', changeSquareNumber);
 bod.prepend(container);
 bod.prepend(btn);
 
-for (let i = 0; i < (squareDiv * squareDiv); i++) {
-    const div = document.createElement('div');
-    div.classList.add('div');
-    div.style.height = 800 / squareDiv + 'px';
-    div.style.width = 800 / squareDiv + 'px';
-    div.addEventListener('mouseover', changeColor);
-    container.appendChild(div);
-}
-
-const div = Array.from(document.querySelectorAll('.div'));
+createSquareDiv();
