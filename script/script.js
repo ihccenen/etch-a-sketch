4546470ab9a20167fb34b339c20c2b6b1be9a566
ihccenen <wirkz01@gmail.com>
2022-09-05 19:33:36 -0300

    Changed colors and change squares numbers

--- script/script.js
+++ script/script.js
@@ -12,36 +12,39 @@ function createSquareDiv(num = 16) {
 
 function changeSquareNumber() {
     const allSquareDiv = document.querySelectorAll('.div') 
-    let squareDiv = prompt('New number of squares per side. Maximum 100.');
+    let squareDiv;
     percent = 0.10;
     
-    while (squareDiv > 100) {
-        squareDiv = prompt('Too many. Maximum 100.')
-    }
+    do {
+        squareDiv = prompt('Change the number of squares per side. Maximum 100.')
+
+        if (!parseInt(squareDiv)) {
+            squareDiv = 16;
+        }
+    } while (squareDiv > 100);
     
     allSquareDiv.forEach(div => div.remove());
-    
-    if (parseInt(squareDiv)) {
-        btn.textContent = `Change the number of squares per side. Current: ${squareDiv}x${squareDiv}`;
-    
-        createSquareDiv(squareDiv);
-    } else {
-        createSquareDiv();
-    }
 
+    btn.textContent = `Change the number of squares per side. Current: ${squareDiv}x${squareDiv}`;
+
+    createSquareDiv(squareDiv);
 }
 
 function changeColor(e) {
+    const divColors = [];
+    
     if (!e.target.classList.contains('color')) {
         let red = Math.round(Math.random() * 225) + 30;
         let green = Math.round(Math.random() * 225) + 30;
         let blue = Math.round(Math.random() * 225) + 30;
-
+        
         e.target.classList.add('color');
-        divColors.splice(0, 3, red - (red * percent), green - (green * percent), blue - (blue * percent));
-
+        divColors.push(red - (red * percent));
+        divColors.push(green - (green * percent));
+        divColors.push(blue - (blue * percent));
+        
         e.target.style.backgroundColor = `rgb(${divColors[0]}, ${divColors[1]}, ${divColors[2]})`;
-
+        
         percent += 0.10;
     }
 }
@@ -50,7 +53,6 @@ const bod = document.querySelector('body');
 const container = document.createElement('div');
 const btn = document.createElement('button');
 const div = Array.from(document.querySelectorAll('.div'));
-const divColors = [];
 let percent = 0.10;
 
 container.classList.add('container');
